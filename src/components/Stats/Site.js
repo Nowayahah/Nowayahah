312f7dd27c2a50c7e641054117d6fe42ec55f30f
nowayahah <thib.danv@hotmail.fr>
2022-01-06 12:34:49 +0100

    run main

--- src/components/Stats/Site.js
+++ src/components/Stats/Site.js
@@ -9,7 +9,7 @@ const Stats = () => {
   const fetchData = useCallback(async () => {
     // request must be authenticated if private
     const res = await fetch(
-      'https://api.github.com/repos/mldangelo/WEB_SITE_REPO',
+      'https://api.github.com/repos/nowayahah/nowayahah',
     );
     const resData = await res.json();
     setResponseData(
